252bd341c12a181f90078319986065f3242e0e2a
alex <alexfacudiaz@gmail.com>
2026-08-13 23:00:39 -0300

    feat(customers): add customer admin

--- backend/customers/admin.py
+++ backend/customers/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'customer_type',
+        'tax_id',
+        'email',
+        'is_active'
+    )
+
+    list_filter = (
+        'customer_type',
+        'is_active'
+    )
+
+    search_fields = (
+        'first_name',
+        'last_name',
+        'business_name',
+        'tax_id',
+        'email'
+    )
+
+    ordering = (
+        'customer_type',
+        'last_name',
+        'business_name'
+    )
+
+    @admin.display(description='Name')
+    def display_name(self, obj):
+        return str(obj)
+
